acb94c542df7d0935e5153cd81b8d9349ac992fa
CSandor2080 <92297111+CSandor2080@users.noreply.github.com>
2024-06-16 04:32:28 +0300

    Update resnet50_train.py

--- resnet50_train.py
+++ resnet50_train.py
@@ -4,18 +4,23 @@ from torchvision import datasets, transforms, models
 import torch.nn as nn
 import torch.optim as optim
 
-from ray.train import  CheckpointConfig,RunConfig,ScalingConfig
-def train_func(config):
+from ray.train import RunConfig, ScalingConfig
+
+def train_func():
+    batch_size = 64
+    lr = 0.001
+    epochs = 5
+    
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
     train_dataset = datasets.MNIST('/data', train=True, download=True, transform=transform)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
     model = models.resnet50(pretrained=False)
     model.fc = nn.Linear(model.fc.in_features, 10)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=config["lr"])
+    optimizer = optim.Adam(model.parameters(), lr=lr)
 
-    for epoch in range(config["epochs"]):
+    for epoch in range(epochs):
         running_loss = 0.0
         for inputs, labels in train_loader:
             optimizer.zero_grad()
@@ -27,21 +32,15 @@ def train_func(config):
         print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
 
 trainer = TorchTrainer(
-    train_func,
+    train_loop_per_worker=train_func,
     scaling_config=ScalingConfig(
         num_workers=2,
         use_gpu=False
     ),
     run_config=RunConfig(
-        checkpoint_config=CheckpointConfig(
-            checkpoint_frequency=1
-        )
-    ),
-    config={
-        "batch_size": 64,
-        "lr": 0.001,
-        "epochs": 5
-    }
+        name="torch_benchmark"
+    )
 )
 
+# Directly start the training job
 trainer.fit()
