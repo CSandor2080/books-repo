ced25832138344ec6c9a3afdf6cb4585f5ade421
CSandor2080 <92297111+CSandor2080@users.noreply.github.com>
2024-06-16 04:16:56 +0300

    Update resnet50_train.py

--- resnet50_train.py
+++ resnet50_train.py
@@ -4,7 +4,9 @@ from torchvision import datasets, transforms, models
 import torch.nn as nn
 import torch.optim as optim
 
-from ray.train import  CheckpointConfig,RunConfig,ScalingConfig
+from ray import tune
+from ray.train import RunConfig, ScalingConfig
+
 def train_func(config):
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
     train_dataset = datasets.MNIST('/data', train=True, download=True, transform=transform)
@@ -27,21 +29,19 @@ def train_func(config):
         print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
 
 trainer = TorchTrainer(
-    train_func,
+    train_loop_per_worker=train_func,
     scaling_config=ScalingConfig(
         num_workers=2,
         use_gpu=False
     ),
     run_config=RunConfig(
-        checkpoint_config=CheckpointConfig(
-            checkpoint_frequency=1
-        )
-    ),
-    config={
-        "batch_size": 64,
-        "lr": 0.001,
-        "epochs": 5
-    }
+        name="torch_benchmark"
+    )
 )
 
-trainer.fit()
+# Start the training job with the specified config using trainer.fit
+trainer.fit(config={
+    "batch_size": 64,
+    "lr": 0.001,
+    "epochs": 5
+})
